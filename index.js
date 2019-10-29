9e77e280b5f35ac5543aa573a6343d9730efb51a
Riccardo Cecchini <rcecchini.ds@gmail.com>
2019-10-29 19:41:42 +0100

    Tutti sbagliano

---
+++ index.js
@@ -0,0 +1,72 @@
+const { app, BrowserWindow, globalShortcut, Menu } = require('electron');
+
+global.appName = "test";
+
+global.shantiDir = __dirname;
+
+class ShantiApp {
+    construct(){
+        
+    }
+    
+    ready(fn){
+        app.on('ready', fn);
+    }
+    
+    loadWindow(file){
+        // https://electronjs.org/docs/api/browser-window
+        // Create the browser window.
+        var win = new BrowserWindow({
+            title: appName,
+            width: 800,
+            height: 600,
+            webPreferences: {
+              nodeIntegration: true
+            },
+            titleBarStyle: 'hidden',
+            frame: false,
+            //transparent: true
+        });
+
+        // and load the index.html of the app.
+        //win.loadFile('index.html')
+        //win.loadFile(file);
+        win.loadFile(shantiDir + '/test.html');
+        
+        // Open the DevTools.
+        //win.webContents.openDevTools()
+
+        // Emitted when the window is closed.
+        win.on('closed', () => {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            win = null
+
+            process.exit();
+        });
+    }
+}
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+
+module.exports = {
+    shantiApp: new ShantiApp()
+};
